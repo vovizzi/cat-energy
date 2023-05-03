c57d7cdc9d1d5ca4142793f2f2868e9852cd7e4d
Vladimir <vovaazoov@rambler.ru>
2023-05-03 17:27:30 +0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("resize", (event) => {
-  if (window.innerWidth <= 820) {
+  if (window.innerWidth <= 420) {
     let a = document.querySelector('img[src="./img/logo-tablet.svg"]');
     if (a) {
       a.setAttribute("src", "./img/logo-mobile.svg");
