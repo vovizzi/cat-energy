d820ada02826cfa5af82036f3fa05c4b0dbb30d5
Vladimir <vovaazoov@rambler.ru>
2023-05-05 17:39:07 +0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-window.addEventListener("resize", (event) => {
+
+function windowSize() { 
   if (window.innerWidth <= 767) {
     let a = document.querySelector('img[src="./img/logo-tablet.svg"]');
     if (a) {
@@ -9,5 +10,21 @@ window.addEventListener("resize", (event) => {
     if (a) {
       a.setAttribute("src", "./img/logo-tablet.svg");
     }
-  }
-});
+  } 
+;
+}
+
+window.addEventListener("resize", windowSize)
+window.addEventListener("load", windowSize())
+
+
+// document.querySelector('.burger').addEventListener('click',function() {
+//   this.classList.toggle('active')
+//   document.querySelector('.header-nav').classList.toggle('.open')
+// })
+
+
+document.querySelector('.burger').addEventListener('click',function() {
+  this.classList.toggle('active')
+  document.querySelector('.header-nav').remove()
+})
